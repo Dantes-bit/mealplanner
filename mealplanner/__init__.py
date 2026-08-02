2c0a316425d63608ded22abaec30e42ec29564be
Dantes-bit <dantessmail@proton.me>
2026-08-02 17:54:24 +0200

    fiks sirkulær import - flytt push_utils import inn i create_app

--- mealplanner/__init__.py
+++ mealplanner/__init__.py
@@ -4,10 +4,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
-from mealplanner.config import Config
-import cloudinary
 from apscheduler.schedulers.background import BackgroundScheduler
-from mealplanner.push_utils import notify_expiring_items
+from mealplanner.config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -20,25 +18,11 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    cloudinary.config(
-        cloud_name=app.config.get('CLOUDINARY_CLOUD_NAME'),
-        api_key=app.config.get('CLOUDINARY_API_KEY'),
-        api_secret=app.config.get('CLOUDINARY_API_SECRET')
-    )
-
-    def run_expiration_check():
-        with app.app_context():
-            notify_expiring_items(days_ahead=2)
-
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
     migrate = Migrate(app, db)
-
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(run_expiration_check, 'cron', hour=9, minute=0)
-    scheduler.start()
 
     from mealplanner.users.routes import users
     from mealplanner.meals.routes import meals
@@ -52,5 +36,15 @@ def create_app(config_class=Config):
     @app.context_processor
     def inject_vapid_key():
         return dict(vapid_public_key=app.config.get('VAPID_PUBLIC_KEY'))
+
+    from mealplanner.push_utils import notify_expiring_items
+
+    def run_expiration_check():
+        with app.app_context():
+            notify_expiring_items(days_ahead=2)
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(run_expiration_check, 'cron', hour=9, minute=0)
+    scheduler.start()
 
     return app
